9bf753d7ed96efd28ec29e77dd88948d36ea63e4
MohammadNR <tntpsnr@gmail.com>
2023-11-30 08:23:05 +0200

    Last edit

--- main.mjs
+++ main.mjs
@@ -4,9 +4,8 @@ import { BinaryTreeNode } from "./BinaryTreeNode.mjs";
 
 // // Example for input as text
 // const string = "mohammad";
-// // const string = "AAABCDEE";
 // const initialValue = string[0];
-// const stringArray = string.split(""); 
+// const stringArray = string.split("");
 // // O(n)
 // stringArray.reduce((accumulator, currentValue) => {
 //   if (data.has(currentValue)) {
@@ -17,7 +16,7 @@ import { BinaryTreeNode } from "./BinaryTreeNode.mjs";
 // }, initialValue);
 
 // Example for input as Priority
- const data = new Map([['A',0.375],['B',0.125],['C',0.125],['D',0.125],['E',0.25]]);
+const data = new Map([['A',0.3],['B',0.2],['C',0.3],['D',0.2]]);
 // console.log(data);
 // This code sort values in data O(n)
 const sortResult = [...data].sort((a, b) => String(b[1]).localeCompare(a[1]));
@@ -27,7 +26,7 @@ const sortNodeResult = sortResult.map((element) => {
   return new BinaryTreeNode(element[0], element[1]);
 });
 
-// This method generate HuffmanTree O(n)
+// This method generate HuffmanTree
 const generateHuffmanTree = (sortNodeResult) => {
   for (let index = sortNodeResult.length - 1; index >= 0; index--) {
     if (index > 0) {
@@ -35,28 +34,15 @@ const generateHuffmanTree = (sortNodeResult) => {
         sortNodeResult[index].value + sortNodeResult[index - 1].value,
         sortNodeResult[index].value + sortNodeResult[index - 1].value
       );
-      node.left = sortNodeResult[index -1];
-      node.right = sortNodeResult[index];
+      node.right = sortNodeResult[index - 1];
+      node.left = sortNodeResult[index];
       sortNodeResult.pop();
       sortNodeResult.pop();
       sortNodeResult.push(node);
-      //sortNodeResult.sort((a, b) => b.value - a.value);
+      sortNodeResult.sort((a, b) => b.value - a.value);
     }
-};
+  };
 }
-
-// const generateHuffmanTree = (sortNodeResult) => {
-//   while (sortNodeResult.length > 1) {
-//     const node = new BinaryTreeNode(
-//       sortNodeResult[sortNodeResult.length - 1].value + sortNodeResult[sortNodeResult.length - 2].value,
-//       sortNodeResult[sortNodeResult.length - 1].frequency + sortNodeResult[sortNodeResult.length - 2].frequency
-//     );
-//     node.right = sortNodeResult.pop();
-//     node.left = sortNodeResult.pop();
-//     sortNodeResult.push(node);
-//     sortNodeResult.sort((a, b) => b.frequency - a.frequency);
-//   }
-// };
 
 // This method print HuffmanTree as table O(n)
 function printHuffmanTree(nodes, path = "") {
@@ -90,4 +76,5 @@ function printTreeStructure(nodes, indent = 0) {
 }
 generateHuffmanTree(sortNodeResult);
 printHuffmanTree(sortNodeResult);
+console.log("------------------------------");
 printTreeStructure(sortNodeResult);
